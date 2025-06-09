81a2f00a2236d0bf845430933103f5897d314950
prabhjot-2 <prabh1146@gmail.com>
2025-06-10 00:01:08 +0530

    feat: implement Input component, enhance Modal styling, and update Login page with Input fields

--- frontend/interview-prep-ai/src/components/Modal.jsx
+++ frontend/interview-prep-ai/src/components/Modal.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-
-const Modal = ({ children, isOpen, onClose , hideHeader}) => {
+const Modal = ({ children, isOpen, onClose, title, hideHeader }) => {
+  if (!isOpen) return null;
   return (
-    <div>
-      Modal
-    </div>
-  )
-}
+    <div className='fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-black/40'>
+      <div className='relative flex flex-col bg-white shadow-lg rounded-lg overflow-hidden'>
 
-export default Modal
+        {!hideHeader && (
+          <div className='flex items-center justify-between p-4 border-b border-gray-200'>
+            <h3 className='md:text-lg font-medium text-gray-900'>{title}</h3>
+          </div>
+        )}
+
+        <button
+          type='button'
+          className='text-gray-400 bg-transparent hover:bg-orange-100 hover:text-gray-900 rounded-lg text-sm w-8 h-8 flex justify-center items-center absolute top-3.5 right-3.5 cursor-pointer'
+          onClick={onClose}
+        >
+          ×
+        </button>
+
+        <div className='flex-1 overflow-y-auto custom-scrollbar'>
+          {children}
+        </div>
+
+      </div>
+    </div>
+  );
+};
+export default Modal;
--- frontend/interview-prep-ai/src/pages/Auth/Login.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/Login.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Input from '../../components/Input/Input'
 
-const Login = () => {
+const Login = ({ setCurrentPage }) => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(null)
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    // Your login logic here
+  };
+
   return (
-    <div>
-      Login
-    </div>
-  )
-}
+    <div className='w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center'>
+      <h3 className='text-lg font-semibold text-black'>Welcome Back</h3>
+      <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+        Please Enter your details to log in
+      </p>
 
-export default Login
+      <form onSubmit={handleLogin}>
+        <Input
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+          label="Email Address"
+          placeholder="john@example.com"
+          type="text"
+        />
+
+        <Input
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+          label="Password"
+          placeholder="Min 8 characters"
+          type="password"
+        />
+
+        {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+
+        <button type='submit' className='btn-primary'>
+          LOGIN
+        </button>
+
+        <p className='text-[13px] text-slate-800 mt-3'>
+          Don't have an Account?{' '}
+          <button
+            className='font-medium text-primary underline cursor-pointer'
+            onClick={() => {
+              setCurrentPage("signup");
+            }}
+          >
+            SignUp
+          </button>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
