01ac267240f0f7ad54b30d32225e2f508d99ab45
prabhjot-2 <prabh1146@gmail.com>
2025-06-12 09:46:38 +0530

    feat: implement user registration and login functionality with JWT, add image upload route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,32 +1,94 @@
-const User=require("../models/User");
-const bcrypt=require("bcryptjs");
-const jwt=require("jsonwebtoken");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // generate jwt token
-const generateToken=(userId)=>{
-    return jwt.sign({id:userId},process.env.JWT_SECRET,{expiresIn:"7d"})
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
 
 // @desc Register a new useer
 // @route POST /api/auth/register
 // @access Public
 
-const registerUser=async(req,res)=>{
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, profileImageUrl } = req.body;
 
+    // check if user is already exist
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    // create new user
+    const user = await User.create({
+      name,
+      email,
+      password: hashPassword,
+      profileImageUrl,
+    });
+
+    // return user data witjh JWT
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "server error", error: error.essage });
+  }
 };
 
 // / @desc login useer
 // @route POST /api/auth/login
 // @access Public
 
-const loginUser=async(req,res)=>{}
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(500).json({ message: "Invali email or password" });
+    }
+    // compare Password
+    const isMatch = await bcrypt.compare(passsword, user.passsword);
+    if (!isMatch) {
+      return res.status(500).json({ message: "invalid email or password" });
+    }
+
+    // return user data with JWT_SECRET
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "server error", error: error.essage });
+  }
+};
 
 // / @desc  user profile
 // @route POST /api/auth/profile
 // @access Private (requires JWT)
 
-const getUserProfile=async(req,res)=>{
+const getUserProfile = async (req, res) => {
+  try {
+    const user=await user.findById(req.user.id).select("-password")
+    if(!user){
+        return res.status(404).json({message:"user not found"})
+    }
+    res.json(user)
+  } catch (error) {
+    res.status(500).json({ message: "server error", error: error.essage });
+  }
+};
 
-}
-
-module.exports={registerUser,loginUser,getUserProfile}
+module.exports = { registerUser, loginUser, getUserProfile };
