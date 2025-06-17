a60584feb6a57585af1e90f28749d120c7f24c04
prabhjot-2 <prabh1146@gmail.com>
2025-06-17 22:59:27 +0530

    improve formatting and error handling in AI controller methods

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -1,7 +1,7 @@
 const {GoogleGenAI}=require("@google/genai");
 const {conceptExplainPrompt, questionAnswerPrompt}=require("../utils/prompts");
 
-const ai=new GoogleGenAI({apiKey:process.env.GEMINI_API_KEY});
+const ai=new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
 
 // desc: generate interview question and answes using gemini
 // route :  POST /api/ai/generate-questions
@@ -22,7 +22,14 @@ const generateInterviewQuestions=async(req,res)=>{
 
         let rawText=response.text;
     // clean it : remove '''json and ''' free beginning and end
-    
+    const cleanedText=rawText.replace(/^```json\s*/, "")//remove starting '''json
+                             .replace(/```$/, "") //emove ending ```
+                             .trim();//remove extra spaces
+
+    // now safe to parse
+    const data=JSON.parse(cleanedText);
+
+    res.status(200).json(data);
     }catch(error){
         res.status(500).json({message:"Failed to generate questions",error:error.message});
     }
@@ -31,6 +38,35 @@ const generateInterviewQuestions=async(req,res)=>{
 // @route POST /api/ai/generate-explanation
 // access private
 
-const generateConceptExplanation=async(req,res)=>{};
+const generateConceptExplanation=async(req,res)=>{
+    try {
+        const{question}=req.body;
+
+        if(!question){
+            return res.status(400).json({message:"missing required fields"});
+        }
+
+        const prompt=conceptExplainPrompt(question);
+
+        const response=await ai.models.generateContent({
+            model:"gemini-2.0-flash-lite",
+            contents:prompt,
+        })
+
+        let rawText=response.text;
+        // clean it: remove ```json and ``` from beginning and ending
+        const cleanedText=rawText.replace(/^```json\s*/,"")//remove starting ```json
+                                  .replace(/```$/,"")//remove ending ```
+                                  .trim(); //remove extra spaces
+        
+        // now safe to parse
+        const data=JSON.parse(cleanedText);
+
+        res.status(200).json(data);
+    
+    } catch (error) {
+        res.status(500).json({message:"Failed to generate questions",error:error.message});
+    }
+};
 
 module.exports={generateConceptExplanation, generateInterviewQuestions};
