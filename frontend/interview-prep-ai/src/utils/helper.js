6dcab391bb8a123d180a54d208610bbc464c1533
prabhjot-2 <prabh1146@gmail.com>
2025-06-19 23:09:15 +0530

    add SummaryCard component; integrate it into DashBoard for session display; enhance helper functions for email validation and initials extraction

--- frontend/interview-prep-ai/src/utils/helper.js
+++ frontend/interview-prep-ai/src/utils/helper.js
@@ -2,3 +2,15 @@ export const validateEmail = (email) => {
     const regx=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email)
 };
+
+export const getInitials =(title)=>{
+    if(!title) return "";
+
+    const words =title.split("");
+    let intials="";
+
+    for(let i=0; i<Math.min(words.length,2);i++){
+        intials+=words[i][0];
+    }
+    return intials.toUpperCase();
+}
