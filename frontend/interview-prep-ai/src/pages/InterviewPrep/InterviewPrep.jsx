ea27621ae02253395bd56f5c39232ec7bd9d7905
prabhjot-2 <prabh1146@gmail.com>
2025-06-23 10:04:50 +0530

    add RoleInfoHeader component; implement session details display in InterviewPrep page

--- frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom'
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+import moment from 'moment';
+import RoleInfoHeader from './components/RoleInfoHeader';
 
 const InterviewPrep = () => {
+  const {sessionId}=useParams();
+
+  const [sessionData,setSessionSata]=useState(null);
+  const[errorMsg,setErrorMsg]=useState("");
+
+  const [openLearnMoreDrawer, setOpenLearnDrawer]=useState(false);
+  const [explanation, setExplanation]=useState(null);
+
+  const [isLoading,setIsLoading]=useState(false);
+  const [isUpdateLoader,setIsUpdateLoader]=useState(false);
+
+  // fetch session data byy session id
+  const fetchSessionDetailsById=async()=>{};
+  
+  // generate concept explanation
+  const generateConceptExplanation=async(question)=>{}
+
+  // pin question
+  const toggleQuestionPinStatus=async(questionId)=>{}
+
+  // addd more question to session
+  const uploadMoreQuestions=async()=>{};
+
+  useEffect(()=>{
+    if(sessionId){
+      fetchSessionDetailsById();
+    }
+  })
+
   return (
-    <div>
-      InterviewPrep
-InterviewPrep
-    </div>
+    <DashboardLayout>
+      <RoleInfoHeader
+        role={sessionData?.role || ""}
+        topicsToFocus={sessionData?.topicsToFocus || ""}
+        experience={sessionData?.experience || ""}
+        questions={sessionData?.questions || ""}
+        description={sessionData?.description || ""}
+        lastUpdated={sessionData?.updatedAt? moment(sessionData.updatedAt).format("Do MMM YYYY"): ""}
+
+      />
+    </DashboardLayout>
   )
 }
 
