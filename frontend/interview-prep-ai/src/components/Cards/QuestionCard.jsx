d2340bd00c1c7fe70dcaf2614ba5dddb6c6dc487
prabhjot-2 <prabh1146@gmail.com>
2025-06-24 09:30:04 +0530

    add AIResponsePreview component; integrate into QuestionCard for displaying answer previews

--- frontend/interview-prep-ai/src/components/Cards/QuestionCard.jsx
+++ frontend/interview-prep-ai/src/components/Cards/QuestionCard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { LuChevronDown, LuPin, LuPinOff, LuSparkles} from "react-icons/lu"
+import AIResponsePreview from '../../pages/InterviewPrep/components/AIResponsePreview';
 
 const QuestionCard = ({
     question,
@@ -26,44 +27,44 @@ const QuestionCard = ({
         setIsExpanned(!isExpanded);
     };
   return <>
-    <div className=''>
-        <div className=''>
-            <div className=''>
-                <span className=''>Q</span>
+    <div className='bg-white rounded-lg mb-4 overflow-hidden py-4 px-5 shadow-xl shadow-gray-100/70 border border-gray-100/60 group'>
+        <div className='flex items-start justify-between cursor-pointer'>
+            <div className='flex items-start gap-3.5'>
+                <span className='text-xs md:text-[15px] font-semibold text-gray-400 leading-[18px]'>Q</span>
 
-                <h3 className='' onClick={toggleExpand}>
+                <h3 className='text-xs md:text-[14px] font-medium text-gray-800 mr-0 md:mr-20' onClick={toggleExpand}>
                     {question}
                 </h3>
             </div>
 
-            <div className=''>
+            <div className='flex items-center justify-emd ml-4 relative'>
                 <div className={`flex ${
                     isExpanded? "md:flex" :"md:hidden group-hover:flex"
                     }`} 
                 >
                         <button 
-                            className=''
+                            className='flex items-center gap-2 text-xs text-indigo-800 font-medium bg-indigo-50 px-3 py-1 mr-2 rouned text-nowrap border border-indigo-50 hover:border-indigo-200 cursor-pointer'
                             onClick={onTogglePin}
                         >
-                            {isPinned ?QuestionCard(<LuPinOff className=""/>)
-                            :(<LuPin className=""/>)
+                            {isPinned ?QuestionCard(<LuPinOff className="text-xs"/>)
+                            :(<LuPin className="text-xs"/>)
                             }
                         </button>
 
                         <button
-                            className=''
+                            className='flex items-center gap-2 text-xs text-cyan-800 font-medium bg-cyan-50 px-3 py-1 mr-2 rouned text-nowrap border border-cyan-50 hover:border-cyan-200 cursor-pointer'
                             onClick={()=>{
                                 setIsExpanned(true);
                                 onLearnMore();
                             }}
                         >
                             <LuSparkles/>
-                            <span className=''>Learn More</span>
+                            <span className='hidden md:block'>Learn More</span>
                         </button>
                 </div>
 
                     <button
-                        className=''
+                        className='text-gray-400 hover:text-gray-500 cursor-pointer'
                         onClick={toggleExpand}
                     >
                         <LuChevronDown
@@ -75,13 +76,13 @@ const QuestionCard = ({
             </div>
         </div>
         
-        <div className=''
+        <div className='overflow-hidden transaction-all duration-300 ease-in-out'
         style={{maxHeight:`${height}px`}}
         >
             <div ref={contentRef}
-            className=''
+            className='mt-4 text-gray-700 bg-gray-50 px-5 py-3 rounded-lg'
             >
-
+                <AIResponsePreview content={answer}/>
             </div>
         </div>
     </div>
---
+++ frontend/interview-prep-ai/src/pages/InterviewPrep/components/AIResponsePreview.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AIResponsePreview = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AIResponsePreview
